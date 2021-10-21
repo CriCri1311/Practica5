950a543d5e113e3773728af92d6c27e97e4daa79
CriCri1311 <cristina110495@hotmail.com>
2021-10-21 16:22:41 +0200

    SUBIDA

---
+++ Ejercicio1.java
@@ -0,0 +1,7 @@
+package ElementosBásicos;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, Cristina!");
+    }
+}
---
+++ Ejercicio2Main.java
@@ -0,0 +1,7 @@
+package ElementosBásicos;
+
+public class Ejercicio2Main {
+    public static void main(String[] args) {
+        System.out.println("¡Hola!");
+    }
+}
