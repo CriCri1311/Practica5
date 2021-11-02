53d7c4f8bed5c70ddc323d66fe198ac03dc13d38
CriCri1311 <cristina110495@hotmail.com>
2021-11-02 19:55:52 +0100

    CambiosySubida

--- Ejercicio2Main.java
+++ Ejercicio2Main.java
@@ -3,5 +3,6 @@ package ElementosBásicos;
 public class Ejercicio2Main {
     public static void main(String[] args) {
         System.out.println("¡Hola!");
-    }
+        System.out.println("Esto es un cambio para la practica 6");
+    "
 }
